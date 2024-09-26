9b15da17d2db5ec27041c2ecd039818b6f972eb7
Luke Tonstad <73957632+LTonstad@users.noreply.github.com>
2024-09-26 16:23:51 -0500

    Update streamlit_app.py
    Moving fruityvice data from json to dataframe

--- streamlit_app.py
+++ streamlit_app.py
@@ -44,6 +44,8 @@ if ingredients_list:
 
         st.success('Your smoothie is ordered!', icon="✅")
 
+# Seperate section to display fruityvice data
 import requests
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
-st.text(fruityvice_response.json())
+# st.text(fruityvice_response.json())
+fv_df = st.dataframe(data = fruityvice_response.json(), use_container_width = True)
